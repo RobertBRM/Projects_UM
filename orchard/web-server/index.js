547f9575d8ee55c29be164e5aceba3d19519dd48
RobertBRM <140127251+RobertBRM@users.noreply.github.com>
2025-01-02 01:57:31 +0000

    new server routes, updates to style.css and new sign_up html file

--- orchard/web-server/index.js
+++ orchard/web-server/index.js
@@ -10,6 +10,24 @@ app.get('/', (req, res) => {
     res.send('<h1>Welcome to My Web App</h1><p>This is served by Node.js!</p>');
 });
 
+// Serve static files from the "public" folder
+app.use(express.static('public'));
+
+// Define routes
+app.get('/Sign_up', (req, res) => {
+    res.sendFile(__dirname + '/public/Sign_up.html');
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/public/login.html');
+});
+
+app.get('/bsignin', (req, res) => {
+    res.sendFile(__dirname + '/public/bsignin.html');
+});
+// Define routes for different trades
+//Continue This part later 
+
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
